c2a8e1462217c157fb58228b26dec6485f290661
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-13 14:33:16 +0200

    createing db

--- server/index.js
+++ server/index.js
@@ -8,4 +8,9 @@ const app = express();
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
-app.use(cors();)
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://axhar:axhar123@cluster0.fbhgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTropology: true})
